74bc43577fa6125944acfa2fb1eac75491b52576
Enzo SOUNOU <e.sounou@student.isartdigital.com>
2017-05-31 18:32:10 +0200

    [RESTART]

--- FBX/main.cpp
+++ FBX/main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 
 #include "GL/glew.h"
-#include "Device.h"
 
 int main(int ac, char** av)
 {
-	Device* device = new Device();
-
-	device->Init();
-
-	device->Run();
-
-	delete device;
 
 	return EXIT_SUCCESS;
 }
